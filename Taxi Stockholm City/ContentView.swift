3e320b03d5394341fec56788c366c34b172dac00
weeknds <cizanraza@icloud.com>
2023-12-20 20:53:22 +0100

    Initial Commit

---
+++ Taxi Stockholm City/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taxi Stockholm City
+//
+//  Created by Cizan Raza on 2023-12-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taxi Stockholm City/Taxi_Stockholm_CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taxi_Stockholm_CityApp.swift
+//  Taxi Stockholm City
+//
+//  Created by Cizan Raza on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct Taxi_Stockholm_CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
