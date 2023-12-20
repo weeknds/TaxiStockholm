7024249f95c58af1b6ef88ac17da241cd9459401
weeknds <cizanraza@gmail.com>
2023-12-20 23:22:23 +0100

    Add all files

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let celsius = 24.0
+let fahrenheit = celsius * 9 / 5 + 32
+print("The current temperature is \(celsius) celsius and \(fahrenheit) fahrenheit.")
+print("\(celsius) n \(fahrenheit)")
